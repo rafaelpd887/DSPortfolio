ef04016a0cb7d62e4be220dc5338754c74a6a57d
rafaelpd887 <129010508+rafaelpd887@users.noreply.github.com>
2023-10-08 19:47:45 -0300

    Update exercício_árvore_de_regressão.R

--- OutrosProjetos/Árvore de Regressão/exercício_árvore_de_regressão.R
+++ OutrosProjetos/Árvore de Regressão/exercício_árvore_de_regressão.R
@@ -1,78 +1,80 @@
-# Instalação de pacotes
-pacotes <- c('tidyverse','rpart','rpart.plot','gtools','Rmisc','scales','viridis','caret','AMR','randomForest','fastDummies','rattle','xgboost','ggpubr','reshape2')
+# Package Installation
+packages <- c('tidyverse','rpart','rpart.plot','gtools','Rmisc','scales','viridis','caret','AMR','randomForest','fastDummies','rattle','xgboost','ggpubr','reshape2')
 
-if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
-  instalador <- pacotes[!pacotes %in% installed.packages()]
-  for(i in 1:length(instalador)) {
-    install.packages(instalador, dependencies = T)
-    break()}
-  sapply(pacotes, require, character = T) 
+if(sum(as.numeric(!packages %in% installed.packages())) != 0){
+  installer <- packages[!packages %in% installed.packages()]
+  for(i in 1:length(installer)) {
+    install.packages(installer, dependencies = TRUE)
+    break()
+  }
+  sapply(packages, require, character = TRUE) 
 } else {
-  sapply(pacotes, require, character = T) 
+  sapply(packages, require, character = TRUE) 
 }
 
-# Carregando a base de dados
+# Loading the dataset
 
 data(tips)
 
 tips %>% head
 
 ############################
-# 1.1 Construindo a árvore #
+# 1.1 Building the Tree #
 ############################
 tree <- rpart(tip~., 
               data=tips,
               control=rpart.control(maxdepth = 4, cp=0))
 
-# Salvando valores preditos (p) e erro (r) no dataset
+# Saving predicted values (p) and error (r) in the dataset
 tips['p'] = predict(tree, tips)
-tips$p %>% tail # olhando a previsão
+tips$p %>% tail # viewing the prediction
 
 tips['r'] = tips$tip - tips$p
 
-# Criando uma função para plotar a árvore
-plot <- function(arvore_){
-  paleta <- scales::viridis_pal(begin=.75, end=1)(20)
-  plot <- rpart.plot::rpart.plot(arvore_,
-                                 box.palette = paleta)}
-# Plotando a árvore
+# Creating a function to plot the tree
+plot <- function(tree_){
+  palette <- scales::viridis_pal(begin=.75, end=1)(20)
+  plot <- rpart.plot::rpart.plot(tree_,
+                                 box.palette = palette)
+}
+# Plotting the tree
 plot(tree)
 
 ###########################################
-# 1.2 Calculando indicadores de avaliação #
-# Cálculo do SQE, QME, SST, QMT e R²      #
+# 1.2 Calculating Evaluation Metrics #
+# Calculation of SSE, MSE, SST, MST, and R² #
 ###########################################
 
-# Criando um função para avaliação 
-metricas <- function(tips_in, p_var, tip_var){
+# Creating a function for evaluation
+metrics <- function(tips_in, p_var, tip_var){
   n <- dim(tips_in)[1]
-  SQE <- sum((tips_in[tip_var] - tips[tip_var])^2)
-  QME <- SQE/n
+  SSE <- sum((tips_in[tip_var] - tips[tip_var])^2)
+  MSE <- SSE/n
   
-  # Cálculo do SSE (Sum of Squares Total)
-  SST <- sum((tips_in[tip_var] - (tips_in[tip_var] %>% sum)/n)**2)
-  QMT <- SST/n
+  # Calculation of SST (Sum of Squares Total)
+  SST <- sum((tips_in[tip_var] - (tips_in[tip_var] %>% sum)/n)^2)
+  MST <- SST/n
   
-  # Cálculo do R-quadrado
-  R_squared <- 1 - SQE/SST
+  # Calculation of R-squared
+  R_squared <- 1 - SSE/SST
   
-  # Imprimindo os resultados
-  cat("Soma dos Quadrados dos Erros (SQE): ", SQE, "\n") 
-  cat("Quadrado Médio dos Erros (QME) : ", QME, "\n")
-  cat("Somatório dos Quadrados Totais (SST): ", SST, "\n") 
-  cat("Quadrado Médio Total (QMT): ", QMT, "\n")
-  cat("R-quadrado (R²): ", R_squared, "\n")
+  # Printing the results
+  cat("Sum of Squares of Errors (SSE): ", SSE, "\n") 
+  cat("Mean Square Error (MSE) : ", MSE, "\n")
+  cat("Sum of Squares Total (SST): ", SST, "\n") 
+  cat("Mean Square Total (MST): ", MST, "\n")
+  cat("R-squared (R²): ", R_squared, "\n")
   
 }
 
-metricas(tips, "p", "tip")
+metrics(tips, "p", "tip")
 
 #################################
-# 1.3 Análise gráfica           #
+# 1.3 Graphical Analysis        #
 #################################
 
-# Função para fazer plot dos valores previstos em x e observados em y e erro representado por cores
-grafico1 <- function(data, x_var, y_var, r_var) {
+# Function to create a scatterplot of predicted values on x and observed values on y with error represented by colors
+plot1 <- function(data, x_var, y_var, r_var) {
   ggplot(data) +
     geom_point(aes(x = !!sym(x_var), y = !!sym(y_var), color = !!sym(r_var))) +
     theme(legend.position="bottom") +
@@ -80,15 +82,15 @@ grafico1 <- function(data, x_var, y_var, r_var) {
     scale_color_viridis_c()
 }
 
-# Plotando
-grafico1(tips, "p", "tip", "r")
+# Plotting
+plot1(tips, "p", "tip", "r")
 
 #######################################
-### Parte 2: Tunando a árvore #########        
+### Part 2: Tuning the Tree #########        
 #######################################
 
 #######################################
-# 2.1 Treinando a árvore sem travas   #
+# 2.1 Training the Tree Without Constraints   #
 #######################################
 
 tree_hm <- rpart(tip~.,
@@ -100,27 +102,27 @@ tree_hm <- rpart(tip~.,
 )
 
 tips['p_hm'] = predict(tree_hm, tips)
-tips$p %>% tail # investigar a previsão
+tips$p %>% tail # investigate the prediction
 tips['r_hm'] = tips$tip - tips$p_hm
 
 ######################################
-# 2.2 Avaliando a árvore hm         ##
+# 2.2 Evaluating the hm Tree        ##
 ######################################
 
-metricas(tips, "p_hm", "tip")
-grafico1(tips, "p_hm", "tip", "r_hm")
+metrics(tips, "p_hm", "tip")
+plot1(tips, "p_hm", "tip", "r_hm")
 
-## n plotaremos essa árvore para evitar travamentos no computador devido a extensão da mesma.
+## We won't plot this tree to avoid computer freezes due to its size.
 
 ######################################
-# 2.3 Complexidade dos caminhos     ##
+# 2.3 Complexity of the Paths     ##
 ######################################
 
 tab_cp <- rpart::printcp(tree_hm)
 rpart::plotcp(tree_hm)
 
 #######################################################################
-# 2.4 Escolhendo o caminho que otimiza a impureza no cross validation #
+# 2.4 Choosing the Path that Optimizes Impurity in Cross Validation #
 #######################################################################
 
 tab_cp[which.min(tab_cp[,'xerror']),]
@@ -134,15 +136,16 @@ tree_tune <- rpart(tip~.,
                    control = rpart.control(cp = cp_min, 
                                            maxdepth = 30)
 )
-# Valores preditos
+# Predicted values
 tips['p_tune'] = predict(tree_tune, tips)
-tips$p %>% tail # investigar a previsão
+tips$p %>% tail # investigate the prediction
 tips['r_tune'] = tips$tip - tips$p_tune
 
 ##############################################
-## 2.5 Avaliando a árvore tunada            ##
+## 2.5 Evaluating the Tuned Tree            ##
 ##############################################
-metricas(tips, "p_tune", "tip")
-grafico1(tips, "p_tune", "tip", "r_tune")
+metrics(tips, "p_tune", "tip")
+plot1(tips, "p_tune", "tip", "r_tune")
 
 plot(tree_tune)
+
