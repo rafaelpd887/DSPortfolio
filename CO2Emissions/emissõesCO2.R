34a65e5034ca6265e06d375d24a7bf3547ca9504
rafaelpd887 <129010508+rafaelpd887@users.noreply.github.com>
2023-10-08 19:15:05 -0300

    Update emissõesCO2.R

--- CO2Emissions/emissõesCO2.R
+++ CO2Emissions/emissõesCO2.R
@@ -1,91 +1,90 @@
-#Transformando a base de dados do formato do Excel para o formato do RStudio
+# Transforming the Excel format dataset to RStudio format
 FuelConsumption <- read.csv("FuelConsumption.csv")
 
-#Salvando a base de dados transformada para o fromato do RStudio
+# Saving the transformed dataset in RStudio format
 save(FuelConsumption, file = "FuelConsumption.RData")
 
-#Visualizando o tipo das variáveis
-glimpse(FuelConsumption) 
+# Viewing the variable types
+glimpse(FuelConsumption)
 
-#Mudando o tipo da variável MODELYEAR
+# Changing the type of the MODELYEAR variable
 FuelConsumption$MODELYEAR <- as.character(FuelConsumption$MODELYEAR)
 FuelConsumption$CYLINDERS <- as.numeric(FuelConsumption$CYLINDERS)
 FuelConsumption$FUELCONSUMPTION_COMB_MPG <- as.numeric(FuelConsumption$FUELCONSUMPTION_COMB_MPG)
 FuelConsumption$CO2EMISSIONS <- as.numeric(FuelConsumption$CO2EMISSIONS)
 
-#Estatísticas das variáveis
+# Variable statistics
 summary(FuelConsumption)
 
-#Categorias das variáveis categóricas
+# Categories of categorical variables
 levels(factor(FuelConsumption$MAKE))
 levels(factor(FuelConsumption$MODEL))
 levels(factor(FuelConsumption$VEHICLECLASS))
 levels(factor(FuelConsumption$TRANSMISSION))
 levels(factor(FuelConsumption$FUELTYPE))
 
-#Tabelas de frequencia das variáveis categóricas
+# Frequency tables of categorical variables
 table(FuelConsumption$MAKE)
 table(FuelConsumption$MODEL)
 table(FuelConsumption$VEHICLECLASS)
 table(FuelConsumption$TRANSMISSION)
 table(FuelConsumption$FUELTYPE)
 
-#Correlações
+# Correlations
 chart.Correlation((FuelConsumption[, c(5, 6, 9:13)]), histogram = TRUE)
 
-#Removendo a coluna MODELYEAR para evitar erros na modelagem
+# Removing the MODELYEAR column to avoid modeling errors
 dropyear <- FuelConsumption[-c(1)]
 
-#Removendo a coluna MODEL para evitar erros no Stepwise
+# Removing the MODEL column to avoid Stepwise errors
 dropyear2 <- dropyear[-c(2)]
 
-
-#Dummização das variáveis categóricas
+# Dummy encoding of categorical variables
 FuelConsumption_dummies <- dummy_columns(.data = dropyear2,
                                     select_columns = "MAKE",
-                                    remove_selected_columns = T,
-                                    remove_most_frequent_dummy = T)
+                                    remove_selected_columns = TRUE,
+                                    remove_most_frequent_dummy = TRUE)
 
 FuelConsumption_dummies <- dummy_columns(.data = FuelConsumption_dummies,
                                          select_columns = "VEHICLECLASS",
-                                         remove_selected_columns = T,
-                                         remove_most_frequent_dummy = T)
+                                         remove_selected_columns = TRUE,
+                                         remove_most_frequent_dummy = TRUE)
 
 FuelConsumption_dummies <- dummy_columns(.data = FuelConsumption_dummies,
                                          select_columns = "TRANSMISSION",
-                                         remove_selected_columns = T,
-                                         remove_most_frequent_dummy = T)
+                                         remove_selected_columns = TRUE,
+                                         remove_most_frequent_dummy = TRUE)
 
 FuelConsumption_dummies <- dummy_columns(.data = FuelConsumption_dummies,
                                          select_columns = "FUELTYPE",
-                                         remove_selected_columns = T,
-                                         remove_most_frequent_dummy = T)
+                                         remove_selected_columns = TRUE,
+                                         remove_most_frequent_dummy = TRUE)
 
-#Modelagem linear com todas as variáveis
+# Linear modeling with all variables
 modelo_FuelConsumption <- lm(formula = CO2EMISSIONS ~ ., data = FuelConsumption_dummies)
 
-#Parâmetros do modelo_FuelConsumption
+# Model parameters
 summary(modelo_FuelConsumption)
 
-#Stepwise
+# Stepwise
 step_FuelConsumption <- step(modelo_FuelConsumption, k = 3.841459)
 
-summary(step_FuelConsumption2)
+summary(step_FuelConsumption)
 
-#VIF e Tolerance
+# VIF and Tolerance
 ols_vif_tol(modelo_FuelConsumption)
 vif(step_FuelConsumption)
 
-#Eliminando FUELCONSUMPTION_COMB para evitar multicolinearidade
+# Removing FUELCONSUMPTION_COMB to avoid multicollinearity
 step_FuelConsumption2 <- update(step_FuelConsumption, CO2EMISSIONS ~ .- FUELCONSUMPTION_COMB, data = FuelConsumption_dummies)
 
-#Stepwise2
+# Stepwise2
 step_FuelConsumption3 <- step(step_FuelConsumption2, k = 3.841459)
 
-#Shapiro-Francia
+# Shapiro-Francia
 sf.test(step_FuelConsumption3$residuals)
 
-#Plotando os resíduos do modelo step_FuelConsumption2 
+# Plotting the residuals of the step_FuelConsumption2 model
 FuelConsumption_dummies %>%
   mutate(residuos = step_FuelConsumption2$residuals) %>%
   ggplot(aes(x = residuos)) +
@@ -93,11 +92,11 @@ FuelConsumption_dummies %>%
                  fill = "#55C667FF", 
                  bins = 15,
                  alpha = 0.6) +
-  labs(x = "Resíduos",
-       y = "Frequências") + 
+  labs(x = "Residuals",
+       y = "Frequencies") + 
   theme_bw()
 
-#Acrescentando uma curva normal teórica para comparação entre as distribuições
+# Adding a theoretical normal curve for distribution comparison
 FuelConsumption_dummies %>%
   mutate(residuos = step_FuelConsumption2$residuals) %>%
   ggplot(aes(x = residuos)) +
@@ -111,65 +110,64 @@ FuelConsumption_dummies %>%
                             sd = sd(step_FuelConsumption2$residuals)),
                 size = 2, color = "grey30") +
   scale_color_manual(values = "grey50") +
-  labs(x = "Resíduos",
-       y = "Frequência") +
+  labs(x = "Residuals",
+       y = "Frequency") +
   theme_bw()
 
-#Breusch-Pagan
+# Breusch-Pagan
 ols_test_breusch_pagan(step_FuelConsumption3)
 
-#Adicionando fitted values e resíduos do modelo 'step_fuelconsumption2'
-#no dataset 'fuelconsumption_dummies'
+# Adding fitted values and residuals of the 'step_fuelconsumption2' model
+# to the 'fuelconsumption_dummies' dataset
 FuelConsumption_dummies$fitted_step <- step_FuelConsumption2$fitted.values
 FuelConsumption_dummies$residuos_step <- step_FuelConsumption2$residuals
 
-#Gráfico que relaciona resíduos e fitted values do modelo 'step_fuelconsumption2'
+# Plot relating residuals and fitted values of the 'step_fuelconsumption2' model
 FuelConsumption_dummies %>%
   ggplot() +
   geom_point(aes(x = fitted_step, y = residuos_step),
              color = "#55C667FF", size = 3) +
-  labs(x = "Fitted Values do Modelo Stepwise",
-       y = "Resíduos do Modelo Stepwise") +
+  labs(x = "Fitted Values of Stepwise Model",
+       y = "Residuals of Stepwise Model") +
   theme_bw()
 
-#Para calcular o lambda de Box-Cox
+# To calculate the Box-Cox lambda
 lambda_BC <- powerTransform(dropyear2$CO2EMISSIONS)
 lambda_BC
 
-#Inserindo o lambda de Box-Cox na nova base de dados para a estimação de um
-#novo modelo
+# Inserting the Box-Cox lambda in the new dataset for the estimation of a new model
 FuelConsumption_dummies$bcCO2EMISSIONS <- (((dropyear2$CO2EMISSIONS ^ lambda_BC$lambda) - 1) / 
                                    lambda_BC$lambda)
 
-#Estimando um novo modelo múltiplo com dummies
+# Estimating a new multiple model with dummies
 modelo_bc_FuelConsumption <- lm(formula = bcCO2EMISSIONS ~ .-CO2EMISSIONS -fitted_step
                            -residuos_step, 
                            data = FuelConsumption_dummies)
 
-#Parâmetros do modelo
+# Model parameters
 summary(modelo_bc_FuelConsumption)
 
-#Aplicando o procedimento Stepwise
+# Applying the Stepwise procedure
 step_bc_FuelConsumption <- step(modelo_bc_FuelConsumption, k = 3.841459)
 
-summary(step_bc_FuelConsumption3)
+summary(step_bc_FuelConsumption)
 
-#VIF
+# VIF
 vif(step_bc_FuelConsumption2)
 
-#Eliminando FUELCONSUMPTION_COMB e FUELCONSUMPTION_CITY para evitar multicolinearidade
+# Removing FUELCONSUMPTION_COMB and FUELCONSUMPTION_CITY to avoid multicollinearity
 step_bc_FuelConsumption2 <- update(step_bc_FuelConsumption, bcCO2EMISSIONS ~ .- FUELCONSUMPTION_COMB - FUELCONSUMPTION_CITY, data = FuelConsumption_dummies)
 
-#Aplicando novo Stepwise após a eliminação das variáveis acima
+# Applying a new Stepwise after removing the above variables
 step_bc_FuelConsumption3 <- step(step_bc_FuelConsumption2)
 
-#Teste de Shapiro-Francia
+# Shapiro-Francia test
 sf.test(step_bc_FuelConsumption3$residuals)
 
-#Diagnóstico de Heterocedasticidade para o Modelo Stepwise com Box-Cox
+# Heteroscedasticity diagnosis for the Stepwise model with Box-Cox
 ols_test_breusch_pagan(step_bc_FuelConsumption3)
 
-#Plotando os resíduos do modelo step_FuelConsumption2 
+# Plotting the residuals of the step_FuelConsumption2 model
 FuelConsumption_dummies %>%
   mutate(residuos = step_bc_FuelConsumption2$residuals) %>%
   ggplot(aes(x = residuos)) +
@@ -177,11 +175,11 @@ FuelConsumption_dummies %>%
                  fill = "#55C667FF", 
                  bins = 15,
                  alpha = 0.6) +
-  labs(x = "Resíduos",
-       y = "Frequências") + 
+  labs(x = "Residuals",
+       y = "Frequencies") + 
   theme_bw()
 
-#Acrescentando uma curva normal teórica para comparação entre as distribuições
+# Adding a theoretical normal curve for distribution comparison
 FuelConsumption_dummies %>%
   mutate(residuos = step_bc_FuelConsumption2$residuals) %>%
   ggplot(aes(x = residuos)) +
@@ -195,29 +193,29 @@ FuelConsumption_dummies %>%
                             sd = sd(step_bc_FuelConsumption2$residuals)),
                 size = 2, color = "grey30") +
   scale_color_manual(values = "grey50") +
-  labs(x = "Resíduos",
-       y = "Frequência") +
+  labs(x = "Residuals",
+       y = "Frequency") +
   theme_bw()
 
 summary(step_bc_FuelConsumption3)
 
-#Inserindo fitted.values do modelo "step_bc_FuelConsumption3' no banco de dados "FuelConsumption_dummies"
+# Inserting fitted.values of the 'step_bc_FuelConsumption3' model in the 'FuelConsumption_dummies' dataset
 FuelConsumption_dummies$fitted_step3 <- step_bc_FuelConsumption3$fitted.values
 
-#Comparando a capacidade preditiva do modelos "step_bc_FuelConsumption2" com o modelo "step_FuelConsumption2"
+# Comparing the predictive capacity of models "step_bc_FuelConsumption2" with "step_FuelConsumption2"
 plot(FuelConsumption_dummies$CO2EMISSIONS ~ FuelConsumption_dummies$fitted_step)
 plot(FuelConsumption_dummies$CO2EMISSIONS ~ FuelConsumption_dummies$fitted_step2)
 plot(FuelConsumption_dummies$CO2EMISSIONS ~ FuelConsumption_dummies$fitted_step3,
-     xlab = "Valores estimados de emissoes de CO2(boxcox)",
-     ylab = "Valores observados de emissoes de CO2")
+     xlab = "Estimated CO2 Emissions (Box-Cox)",
+     ylab = "Observed CO2 Emissions")
 
-#Transformando de volta valores previstos do modelo Box-Cox e os colocando no dateset FuelConsumption_dummies
+# Transforming back predicted values of the Box-Cox model and putting them in the 'FuelConsumption_dummies' dataset
 FuelConsumption_dummies$yhat_step_bc_FuelConsumption3 <- (((step_bc_FuelConsumption3$fitted.values*(lambda_BC$lambda))+
                                     1))^(1/(lambda_BC$lambda))
 
 plot(FuelConsumption_dummies$CO2EMISSIONS ~ FuelConsumption_dummies$yhat_step_bc_FuelConsumption3,
-     xlab = "Valores estimados de emissoes de CO2(boxcox)",
-     ylab = "Valores observados de emissoes de CO2")
+     xlab = "Estimated CO2 Emissions (Box-Cox)",
+     ylab = "Observed CO2 Emissions")
 
 
 
